f24970dd10d64eb1bf046fc96aaf4cdb090509d1
OGGYBOY09 <andhikaaryaputra13@gmail.com>
2026-01-26 13:28:11 +0700

    bakso tanpa tepung

--- mscompApp/src/mscompapp/PKelUser.java
+++ mscompApp/src/mscompapp/PKelUser.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
 package mscompapp;
+import config.Koneksi;
 import java.sql.Connection;        // Untuk koneksi ke database
 import java.sql.PreparedStatement; // Untuk menjalankan query SQL yang aman
 import java.sql.ResultSet;         // Untuk menampung hasil data dari database
